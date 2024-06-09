ace3faf5b068207a80517b3653b96aab50b23cb2
shuaibasghar <shuaibasghargtk@gmail.com>
2024-06-09 17:29:21 +0500

    Updating Object State Immutably able to get x on board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,24 +6,46 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null],
 ];
+/**
+ important note
+ 
+ UPdate the object-state immutably
+
+ Object and array (which are technically are objects) are reference values in JavaScript
+
+ You should therefore not mutate them directly-- instead create a deep copy first and then update the copy
+
+ */
 
 // Define the GameBoard component
 export default function GameBoard() {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
   function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {});
+    // console.log(rowIndex, colIndex);
+    setGameBoard((prevGameBoard) => {
+      // prevGameBoard[rowIndex][colIndex] = "X"; // this is mutating the state directly not a good practice
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      console.log(updatedBoard);
+      updatedBoard[rowIndex][colIndex] = "X";
+
+      return updatedBoard;
+    });
   }
   return (
     // Render an ordered list with the id "game-board"
     <ol id="game-board">
       {/* Map over each row in the game board */}
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           {/* Map over each square in the row */}
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
