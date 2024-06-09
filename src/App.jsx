9ba47586f2047cf8c21bcadb2581730a140eb45e
shuaibasghar <shuaibasghargtk@gmail.com>
2024-06-09 21:44:45 +0500

    restart game

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState("X");
   const activePlayer = derivedActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -67,6 +67,10 @@ function App() {
       return updateTurns;
     });
   }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
   return (
     <main>
       <div id="game-container">
@@ -82,7 +86,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
         <GameBoard
           // turns={gameTurns}
           board={gameBoard}
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function GameOver({ winner }) {
+function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p>It&apos;s a draw!</p>}
-      <button>Rematch</button>
+      <button onClick={onRestart}>Rematch</button>
     </div>
   );
 }
